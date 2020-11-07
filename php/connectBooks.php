8db71b7b56ce3b00ce7134d437f07214c9338f41
LAPTOP-6FTBK1C5\hare0 <windsign02@gmail.com>
2020-11-08 01:49:52 +0800

    記得改密碼

--- php/connectBooks.php
+++ php/connectBooks.php
@@ -1,7 +1,7 @@
 <?
     $dsn = "mysql:localhost;port=3306;dbname=ed103g3;charset=utf8";
     $user = "root";
-    $password = "si832gn11d";
+    $password = "改自己的喔";
     $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_CASE=>PDO::CASE_NATURAL);
     $pdo = new PDO($dsn,$user,$password ,$options);
 ?>
