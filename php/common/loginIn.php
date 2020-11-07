efd789df74556bda7e5e977a3574903a81bd5f99
LAPTOP-6FTBK1C5\hare0 <windsign02@gmail.com>
2020-11-08 01:11:51 +0800

    增加登入註冊(每頁)

---
+++ php/common/loginIn.php
@@ -0,0 +1,32 @@
+<?php
+try{
+  require_once("../connectBooks.php");
+  $sql = "select * from `member` where MEM_ID=:memId and MEM_PSW=:memPsw"; 
+  $member = $pdo->prepare($sql); //先編譯好
+  
+  $member->bindValue(":memId", $_POST["MEM_ID"]); //代入資料
+	$member->bindValue(":memPsw", $_POST["MEM_PSW"]);
+	$member->execute();//執行之
+  if( $member->rowCount()==0){ //查無此人
+	  echo "{}";
+  }else{ //登入成功
+    //自資料庫中取回資料
+  	$memRow = $member->fetch(PDO::FETCH_ASSOC);
+    //--------------將登入者的資料寫入session
+    session_start();
+    $_SESSION["MEM_ID"] = $memRow["MEM_ID"];
+    $_SESSION["MEM_NAME"] = $memRow["MEM_NAME"];
+    $_SESSION["MEM_NICKNAME"] = $memRow["MEM_NICKNAME"];
+
+  	$result = array("MEM_ID"=>$memRow["MEM_ID"], "MEM_NAME"=>$memRow["MEM_NAME"], "MEM_NICKNAME"=>$memRow["MEM_NICKNAME"]);
+  	$json = json_encode($result);
+
+    //送出登入者的相關資料
+    echo $json;
+  }
+}catch(PDOException $e){
+  $error = array("errorMsg"=>$e->getMessage());
+  echo json_encode($error);//{"errorMsg":"......"}傳回錯誤訊息
+  // echo $e->getMessage();
+}
+?>
--- php/connectBooks.php
+++ php/connectBooks.php
@@ -1,7 +1,7 @@
 <?
-    $dsn = "mysql:host=localhost;port=3306;dbname=ed103g3;charset=utf8";
+    $dsn = "mysql:localhost;port=3306;dbname=ed103g3;charset=utf8";
     $user = "root";
-    $password = "放自己的密碼，謝謝";
+    $password = "si832gn11d";
     $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_CASE=>PDO::CASE_NATURAL);
     $pdo = new PDO($dsn,$user,$password ,$options);
 ?>
