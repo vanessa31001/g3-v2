515636316da3be84fd3304889c80dd9c0d76fdd7
kejinchen <cclaire0820@gmail.com>
2020-11-08 21:55:46 +0800

    shansen group ok

--- php/group/groupindex.php
+++ php/group/groupindex.php
@@ -3,7 +3,7 @@ try{
   require_once("../connectBooks.php");
 
   // $sql = "select * from campinggroups";
-  $sql = "SELECT GROUP_NAME, date(GROUP_START_DATE) `開團日期`, IFNULL(收藏數,0) `收藏數`
+  $sql = "SELECT GROUP_NO `GROUP_NO`,GROUP_NAME, date(GROUP_START_DATE) `開團日期`, IFNULL(收藏數,0) `收藏數`, GROUP_PIC1 `pic`
   FROM campinggroups a JOIN CAMPING b on a.GROUP_CAM_NO = b.CAM_NO
   left join 
   (select CAMPCO_CAMNO , count(*) `收藏數` from campcolloection group by CAMPCO_CAMNO)
@@ -11,14 +11,9 @@ try{
   where GROUP_STATUS = 0
   order by 收藏數 desc";
   $products = $pdo->query($sql);
-  // $groupRow = $products->fetch(PDO::FETCH_ASSOC);
-  // $result = array("GROUP_NAME"=>$groupRow["GROUP_NAME"],"GROUP_START_DATE"=>$groupRow["開團日期"],"likeNum"=>$groupRow["收藏數"]);
-  // echo json_encode($result);
   $result=[];
   while( $groupRow = $products->fetch(PDO::FETCH_ASSOC)){
-    // echo "團名:".$groupRow["GROUP_NAME"]. "<br>開團日期:". $groupRow["開團日期"] ."<br>收藏數:". $groupRow["收藏數"]."<br>";
-    // echo "<br>";
-    $result[] = array("GROUP_NAME"=>$groupRow["GROUP_NAME"],"GROUP_START_DATE"=>$groupRow["開團日期"],"likeNum"=>$groupRow["收藏數"]);
+    $result[] = array("GROUP_NO"=>$groupRow["GROUP_NO"],"GROUP_NAME"=>$groupRow["GROUP_NAME"],"GROUP_START_DATE"=>$groupRow["開團日期"],"likeNum"=>$groupRow["收藏數"], "GROUP_PIC1"=>$groupRow["pic"]);
   }
   echo json_encode($result);
    
