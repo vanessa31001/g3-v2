f47b0ebeec76d3c667bf347bfa5896eb26ff419e
vanessa <vanessa31001@gmail.com>
2020-11-09 19:48:54 +0800

    熱門營區php done

--- php/backstage/backstage_reportgroup.php
+++ php/backstage/backstage_reportgroup.php
@@ -2,7 +2,9 @@
 try{
     header("Access-Control-Allow-Origin: *");
     require_once("../connectBooks.php");
-    $sql = "SELECT * FROM reportgroup "; 
+    $sql = "SELECT re.REGROUP_NO, c.GROUP_NAME, m.MEM_NAME, re.REGROUP_STATUS, re.REGROUP_RESON, c.GROUP_STATUS
+    FROM reportgroup re JOIN member m ON (re.REGROUP_MEMNO = m.MEMNO)
+                        JOIN campinggroups c ON (c.GROUP_NO = re.REGROUP_GROUP_NO)"; 
     $reportgroup = $pdo->prepare($sql);
     $reportgroup->execute();
 
