abb850ae9866d0ac10e2b6151ee03437c8ce1812
kejinchen <cclaire0820@gmail.com>
2020-11-09 20:49:44 +0800

    select php

--- php/group/area.php
+++ php/group/area.php
@@ -1,13 +1,20 @@
-`<?php
+<?php
 try{
   require_once("../connectBooks.php");
   // $sql = "select * from campinggroups";
-  $area = 
-  $sql = "select cam_no, cam_name, cam_area from camping where cam_area = :area;";
+  if($_GET["CAM_AREA"]==1){
+    $cond = 1;
+  }else{
+    $AREA=$_GET["CAM_AREA"];
+    $cond = "CAM_AREA = '{$_GET["CAM_AREA"]}'";
+    // echo ($cond); 
+  }
+  // $area = $_GET["CAM_AREA"];
+  $sql = "select CAM_NO, CAM_NAME, CAM_AREA, CAM_COUNTY from camping where $cond";
   $products = $pdo->query($sql);
   $result=[];
   while( $groupRow = $products->fetch(PDO::FETCH_ASSOC)){
-    $result[] = array("GROUP_NO"=>$groupRow["GROUP_NO"],"GROUP_NAME"=>$groupRow["GROUP_NAME"],"GROUP_START_DATE"=>$groupRow["開團日期"],"likeNum"=>$groupRow["收藏數"], "GROUP_PIC1"=>$groupRow["pic"]);
+    $result[] = array("CAM_NO"=>$groupRow["CAM_NO"],"CAM_NAME"=>$groupRow["CAM_NAME"],"CAM_AREA"=>$groupRow["CAM_AREA"],"CAM_COUNTY"=>$groupRow["CAM_COUNTY"]);
   }
   echo json_encode($result);
    
