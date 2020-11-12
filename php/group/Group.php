f0fe27d808d2c2c765125acad39f16e0c8dc5057
kejinchen <cclaire0820@gmail.com>
2020-11-12 22:07:07 +0800

    still trying

--- php/group/Group.php
+++ php/group/Group.php
@@ -1,8 +1,13 @@
 <?php
 try{
   require_once("../connectBooks.php");
-  $i = $_GET["loca"];
-  switch($i){
+  $loca = $_GET["loca"];
+  $people = $_GET["people"];
+  echo $people;
+  $date = $_GET["date"];
+  echo $date;
+
+  switch($loca){
     case "1":
       $cond1 = "c.CAM_AREA='北部'";
       break; 
@@ -19,6 +24,8 @@ try{
       $cond1 = 1;
   }
 
+  
+
   $sql = "SELECT a.GROUP_NO `團編號`, a.GROUP_MEMNO `會員編號`, b.MEM_NICKNAME `會員名`,b.MEM_IMG `會員照片`, a.GROUP_PIC1 `圖片1`, a.GROUP_NAME `團名`, a.GROUP_INTRO `揪團介紹`, c.CAM_AREA  `地區`, c.CAM_COUNTY `縣市` , c.CAM_NAME  `營地`,
   a.GROUP_PEOPLE_LIMIT  `人數上限`, a.GROUP_PEOPLE_SIGNUP  `參團人數`, (a.GROUP_PEOPLE_LIMIT - a.GROUP_PEOPLE_SIGNUP)  `剩餘名額`, date(a.GROUP_START_DATE)  `開團日`,
   date(a.GROUP_DEPART_DATE)  `出發日`, date(a.GROUP_DEADLINE)  `回程日`, a.GROUP_STATUS  `團目前狀態`, a.GROUP_REASON , IFNULL(收藏數,0) `收藏數`
@@ -29,7 +36,7 @@ try{
   order by GROUP_START_DATE desc;";
   $groupRows = $pdo->query($sql);
   $groupRow = $groupRows->fetchAll(PDO::FETCH_ASSOC);
-  echo json_encode($groupRow);   
+  // echo json_encode($groupRow);   
     
 }catch(PDOException $e){
 	$error = array("errorMsg"=>$e->getMessage());
