77670b15197ea916021040afd19574761af9c039
LAPTOP-6FTBK1C5\hare0 <windsign02@gmail.com>
2020-11-13 03:18:30 +0800

    會員揪團相關完成

--- php/common/loginOut.php
+++ php/common/loginOut.php
@@ -1,6 +1,7 @@
 <?
     //移除資訊
     session_start();
+    unset($_SESSION["MEMNO"]);
     unset($_SESSION["MEM_ID"]);
     unset($_SESSION["MEM_NAME"]);
     unset($_SESSION["MEM_NICKNAME"]);
--- php/common/registered.php
+++ php/common/registered.php
@@ -15,7 +15,14 @@ try{
         $member2->bindValue(":MEM_NICKNAME", $_POST["MEM_NICKNAME"]);
         $member2->execute();//執行之
 
+        $sql3 = "select * from `member` where MEM_ID=:MEM_ID";
+        $member3 = $pdo->prepare($sql3); //先編譯好
+        $member3->bindValue(":MEM_ID", $_POST['MEM_ID']);
+        $member3->execute();//執行之
+        $memRow = $member3->fetch(PDO::FETCH_ASSOC);
+
         session_start();
+        $_SESSION["MEMNO"] = $memRow["MEMNO"];
         $_SESSION["MEM_ID"] = $_POST["MEM_ID"];
         $_SESSION["MEM_NAME"] = $_POST["MEM_NAME"];
         $_SESSION["MEM_NICKNAME"] = $_POST["MEM_NICKNAME"];
