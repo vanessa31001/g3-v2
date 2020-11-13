b206fc1f04dd2612119d39d8d7968baab70b7aa8
vanessa <vanessa31001@gmail.com>
2020-11-13 15:51:57 +0800

    暫存一下

--- js/like.js
+++ js/like.js
@@ -1,5 +1,11 @@
+// window.addEventListener('load',function(){
+//     let CAM_NO = location.href.split('?')[1];
+//     spotNum(CAM_NO);
+// })
+let CAM_NO = location.href.split('?')[1];
 function addLike(){
-    let forum_post_no = this.reflection[0].forum_post_no;
+    // console.log(CAM_NO);
+    alert(CAM_NO);
     let addlikexhr = new XMLHttpRequest();
 
     addlikexhr.onload = function() {
@@ -17,14 +23,14 @@ function addLike(){
             var url = "php/common/addlike.php";
             xhr.open("post", url, true);
             xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded")
-            let data = `forum_post_no=${forum_post_no}`;
+            let data = `CAM_NO=${CAM_NO}`;
             xhr.send(data);
 
-            if ($(".heart").attr('src') === "./images/icons/icon_heart.svg") {
-                $(".heart").attr("src", "./images/icons/icon_heart_h&c.svg");
-            } else {
-                $(".heart").attr("src", "./images/icons/icon_heart.svg");
-            }
+            // if ($(".heart").attr('src') === "./images/icons/icon_heart.svg") {
+            //     $(".heart").attr("src", "./images/icons/icon_heart_h&c.svg");
+            // } else {
+            //     $(".heart").attr("src", "./images/icons/icon_heart.svg");
+            // }
         } else {
             //沒有登入，請先登入
             alret("請先登入")
--- php/common/addlike.php
+++ php/common/addlike.php
@@ -1,21 +1,21 @@
 <?php
-  require_once("../connectBooks.php");
-  $sql = "INSERT INTO dept(cc.CAMPCO_MEMNO, cc.CAMPCO_CAMNO)
-  SELECT cc.CAMPCO_MEMNO, cc.CAMPCO_CAMNO
-  FROM campcolloection cc JOIN camping c ON (cc.CAMPCO_CAMNO = c.CAM_NO)
-                          JOIN member m ON (m.MEMNO = cc.CAMPCO_MEMNO)
-  WHERE m.MEM_ID = ':MEMID'
-  "; 
+session_start();
+require_once("../connectBooks.php");
+$sql = "INSERT INTO dept(cc.CAMPCO_MEMNO, cc.CAMPCO_CAMNO)
+SELECT cc.CAMPCO_MEMNO, cc.CAMPCO_CAMNO
+FROM campcolloection cc JOIN camping c ON (cc.CAMPCO_CAMNO = c.CAM_NO)
+                        JOIN member m ON (m.MEMNO = cc.CAMPCO_MEMNO)
+WHERE m.MEM_ID = ':MEMID'"; 
   $member = $pdo->prepare($sql); //先編譯好
-  
+
   $member->bindValue(":memId", $_POST["MEM_ID"]); //代入資料
 	$member->bindValue(":memPsw", $_POST["MEM_PSW"]);
 	$member->execute();//執行之
   if( $member->rowCount()==0){ //查無此人
-	  echo "{}";
+	echo "{}";
   }else{ //登入成功
     //自資料庫中取回資料
-  	$memRow = $member->fetch(PDO::FETCH_ASSOC);
+    $memRow = $member->fetch(PDO::FETCH_ASSOC);
     //--------------將登入者的資料寫入session
     session_start();
     $_SESSION["MEM_ID"] = $memRow["MEM_ID"];
