b38ee01271203e0b45553e653a68295125ea4054
vanessa <vanessa31001@gmail.com>
2020-11-15 10:34:43 +0800

    收藏Done

--- php/camping/ifliked1.php
+++ php/camping/ifliked1.php
@@ -6,30 +6,23 @@ try{
     FROM member m
     join campcolloection c
     ON (m.MEMNO=c.CAMPCO_MEMNO)
-    WHERE m.MEMNO = :CAMPCO_MEMNO"; 
+    WHERE m.MEMNO = :CAMPCO_MEMNO AND c.CAMPCO_CAMNO = :CAMPCO_CAMNO "; 
 
     
-
-    // var_dump( $_SESSION["MEMNO"]);
-    // die;
     $campcolloection = $pdo->prepare($sql);
-    // $campcolloection->bindValue(":CAMPCO_MEMNO", $_SESSION["MEMNO"]);
     $campcolloection->bindValue(":CAMPCO_MEMNO", $_SESSION["MEMNO"]);
+    $campcolloection->bindValue(":CAMPCO_CAMNO", $_POST["CAM_NO"]);
     $campcolloection->execute();
-    // $arr ='';
+
     if($campcolloection->rowCount() == 0 ){
-        // $arr = './pic/icon/like.png';//灰色愛心
         echo '沒收藏';
     }else{
-        // $arr = './pic/icon/like_check.png';//紅色愛心
         echo '有收藏';
     }
-    
-    $campcolloectionRow = $campcolloection->fetch(PDO::FETCH_ASSOC);
-    // array_push($campcolloectionRow,$arr);
-    // var_dump($campcolloectionRow);
+    // var_dump($campcolloection);
     // die;
-    // echo json_encode ($campcolloectionRow);
+    // $campcolloectionRow = $campcolloection->fetch(PDO::FETCH_ASSOC);
+
 
 }catch(PDOException $e){
         echo "錯誤訊息:", $e->getLine(),"<br>";
