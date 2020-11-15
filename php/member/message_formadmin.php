2599947a00418d2f4fa3a50f88c4df4efc891c6d
LAPTOP-6FTBK1C5\hare0 <windsign02@gmail.com>
2020-11-15 14:33:31 +0800

    和SANDY的合併(尚未寫入資料庫

---
+++ php/member/message_formadmin.php
@@ -0,0 +1,19 @@
+<?php 
+require_once("../connectBooks.php");
+
+// $cond1 = "MES_OBJECT_MEMNO={$_GET["MES_OBJECT_MEMNO"]}";
+// $cond2 = "MES_SENDER_MEMNO={$_GET["MES_SENDER_MEMNO"]}";
+
+// $sql = "select * from message where $cond1 and $cond2 ";
+
+$sql = "SELECT mes.MES_CONTENT, m.MEM_NAME, m.MEMNO, m.MEM_IMG, mes.MES_NO
+FROM message mes JOIN member m ON (mes.MES_OBJECT_MEMNO = m.MEMNO)
+WHERE mes.MES_OBJECT_MEMNO IN (SELECT mes1.MES_SENDER_MEMNO
+                      FROM message mes1 JOIN  member m1 ON (mes1.MES_SENDER_MEMNO = m1.MEMNO)
+                      WHERE m1.MEM_ID ='" . $_GET['memail'] . "')";
+                     
+
+$message = $pdo->query($sql);//執行sql
+$mesRows = $message->fetchAll(PDO::FETCH_ASSOC);//撈執行完的結果
+echo json_encode($mesRows);//回傳給.html
+?>
