ff9b36f063b1d84980546ececf83ca96e37caae9
LAPTOP-6FTBK1C5\hare0 <windsign02@gmail.com>
2020-11-16 20:02:05 +0800

    會員改到一半

--- php/connectBooks.php
+++ php/connectBooks.php
@@ -1,7 +1,7 @@
 <?php
     $dsn = "mysql:host=localhost;port=3306;dbname=ed103g3;charset=utf8";
     $user = "root";
-    $password = "vanessa";
+    $password = "si832gn11d";
     $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_CASE=>PDO::CASE_NATURAL);
     $pdo = new PDO($dsn,$user,$password ,$options);
 ?>
