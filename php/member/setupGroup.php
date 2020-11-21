bd4595fd92a88d52b70e524d9dd8c5405c3df2c4
LAPTOP-6FTBK1C5\hare0 <windsign02@gmail.com>
2020-11-21 16:31:33 +0800

    bug修正

--- php/member/setupGroup.php
+++ php/member/setupGroup.php
@@ -5,7 +5,7 @@
             header("Access-Control-Allow-Origin: *");
             require_once("../connectBooks.php");
             $sql = 
-            "SELECT GROUP_NO,GROUP_NAME,GROUP_INTRO,CAM_NAME,date_format(group_depart_date,'%Y-%m-%d') 'group_Date',GROUP_PEOPLE_SIGNUP,GROUP_PIC1,MEM_NAME 
+            "SELECT GROUP_NO,GROUP_NAME,GROUP_INTRO,CAM_NAME,date_format(group_depart_date,'%Y-%m-%d') 'group_Date',GROUP_PEOPLE_SIGNUP,GROUP_PIC1,MEM_NAME,GROUP_STATUS 
             FROM campinggroups c join member m on c.GROUP_MEMNO=m.MEMNO
                                  join camping on c.GROUP_CAM_NO=camping.CAM_NO
             where m.MEM_ID=:memEmail";
@@ -14,10 +14,13 @@
             $member->execute();
             $members = $member->fetchAll(PDO::FETCH_ASSOC);
             $arr =[];
+            // var_dump($members);
+            // die;
             foreach($members as $key => $val){
                 $nowTime = time();
                 $groupTime = strtotime($members[$key]['group_Date']);
-                if($nowTime < $groupTime){
+                $status = $members[$key]['GROUP_STATUS'];
+                if($nowTime < $groupTime && $status != '3'){
                     $val['status'] = '0';
                 }else{
                     $val['status'] = '1';
